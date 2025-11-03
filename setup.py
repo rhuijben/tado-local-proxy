36a75fac20b9364fc33ddfb6fa5543479c8efa5c
Bert Huijben <bert@qqmail.nl>
2025-11-03 09:30:13 +0100

    Resolve python version confusion

--- setup.py
+++ setup.py
@@ -50,14 +50,12 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
         "Topic :: Home Automation",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
-    python_requires=">=3.9",
+    python_requires=">=3.11",
     install_requires=requirements,
     entry_points={
         "console_scripts": [
